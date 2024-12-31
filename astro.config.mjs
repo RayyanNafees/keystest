69ecb146dadbba1ef79b7b664b8a8ae9cd59a4c4
9node <rayyan@theninthnode.com>
2024-12-31 16:06:44 +0530

    added netlify build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
-import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 });
